qdrant/qdrant

OptimizationPlanner

lib/collection/src/collection_manager/optimizers/config_mismatch_optimizer.rs
@@ -1,18 +1,16 @@
-use std::collections::HashSet;
+use std::cmp::Reverse;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
+use itertools::Itertools;
 use parking_lot::Mutex;
 use segment::common::operation_time_statistics::OperationDurationsAggregator;
 use segment::entry::SegmentEntry;
 use segment::index::sparse_index::sparse_index_config::SparseIndexType;
-use segment::types::{
-    HnswConfig, HnswGlobalConfig, Indexes, QuantizationConfig, SegmentType, VectorName,
-};
+use segment::types::{HnswConfig, HnswGlobalConfig, Indexes, QuantizationConfig, VectorName};
 
-use crate::collection_manager::holders::segment_holder::{LockedSegmentHolder, SegmentId};
 use crate::collection_manager::optimizers::segment_optimizer::{
-    OptimizerThresholds, SegmentOptimizer,
+    OptimizationPlanner, OptimizerThresholds, SegmentOptimizer,
 };
 use crate::config::CollectionParams;
 use crate::operations::config_diff::DiffConfig;
@@ -75,10 +73,6 @@ impl ConfigMismatchOptimizer {
 
     fn config_mismatch(&self, read_segment: &dyn SegmentEntry) -> bool {
         let segment_config = read_segment.config();
-
-        if read_segment.segment_type() == SegmentType::Special {
-            return false; // Never optimize already optimized segment
-        }
 
         if self.collection_params.on_disk_payload
             != segment_config.payload_storage_type.is_on_disk()
@@ -209,19 +203,12 @@ impl SegmentOptimizer for ConfigMismatchOptimizer {
         &self.thresholds_config
     }
 
-    fn check_condition(
-        &self,
-        segments: LockedSegmentHolder,
-        excluded_ids: &HashSet<SegmentId>,
-    ) -> Vec<SegmentId> {
-        segments
-            .read()
+    fn plan_optimizations(&self, planner: &mut OptimizationPlanner) {
+        let to_optimize = planner
+            .remaining()
             .iter()
-            // Excluded externally, might already be scheduled for optimization
-            .filter(|(segment_id, _segment)| !excluded_ids.contains(segment_id))
-            .filter_map(|(segment_id, segment)| {
-                let segment_entry = segment.get();
-                let read_segment = segment_entry.read();
+            .filter_map(|(&segment_id, segment)| {
+                let read_segment = segment.read();
                 self.config_mismatch(&*read_segment).then(|| {
                     let vector_size = read_segment
                         .max_available_vectors_size_in_bytes()
@@ -229,11 +216,12 @@ impl SegmentOptimizer for ConfigMismatchOptimizer {
                     Some((segment_id, vector_size))
                 })?
             })
-            // Select segment with largest vector size
-            .max_by_key(|(_segment_id, vector_size)| *vector_size)
-            .map(|(segment_id, _vector_size)| segment_id)
-            .into_iter()
-            .collect()
+            // Segments with largest vector size come first
+            .sorted_by_key(|(_segment_id, vector_size)| Reverse(*vector_size))
+            .collect_vec();
+        for (segment_id, _) in to_optimize {
+            planner.plan(vec![segment_id]);
+        }
     }
 
     fn get_telemetry_counter(&self) -> &Mutex<OperationDurationsAggregator> {
@@ -365,7 +353,7 @@ mod tests {
 
         // Mismatch optimizer should not optimize yet, HNSW config is not changed yet
         let suggested_to_optimize =
-            config_mismatch_optimizer.check_condition(locked_holder.clone(), &Default::default());
+            config_mismatch_optimizer.plan_optimizations_for_test(&locked_holder);
         assert_eq!(suggested_to_optimize.len(), 0);
 
         // Create changed HNSW config with other m/ef_construct value, update it in the optimizer
@@ -377,7 +365,8 @@ mod tests {
         // Run mismatch optimizer again, make sure it optimizes now
         let permit = budget.try_acquire(0, permit_cpu_count).unwrap();
         let suggested_to_optimize =
-            config_mismatch_optimizer.check_condition(locked_holder.clone(), &Default::default());
+            config_mismatch_optimizer.plan_optimizations_for_test(&locked_holder);
+        let suggested_to_optimize = suggested_to_optimize.into_iter().exactly_one().unwrap();
         assert_eq!(suggested_to_optimize.len(), 1);
         let changed = config_mismatch_optimizer
             .optimize(
@@ -527,7 +516,7 @@ mod tests {
 
         // Mismatch optimizer should not optimize yet, HNSW config is not changed yet
         let suggested_to_optimize =
-            config_mismatch_optimizer.check_condition(locked_holder.clone(), &Default::default());
+            config_mismatch_optimizer.plan_optimizations_for_test(&locked_holder);
         assert_eq!(suggested_to_optimize.len(), 0);
 
         // Create changed HNSW config for vector2, update it in the optimizer
@@ -547,7 +536,8 @@ mod tests {
         // Run mismatch optimizer again, make sure it optimizes now
         let permit = budget.try_acquire(0, permit_cpu_count).unwrap();
         let suggested_to_optimize =
-            config_mismatch_optimizer.check_condition(locked_holder.clone(), &Default::default());
+            config_mismatch_optimizer.plan_optimizations_for_test(&locked_holder);
+        let suggested_to_optimize = suggested_to_optimize.into_iter().exactly_one().unwrap();
         assert_eq!(suggested_to_optimize.len(), 1);
         let changed = config_mismatch_optimizer
             .optimize(
@@ -703,7 +693,7 @@ mod tests {
 
         // Mismatch optimizer should not optimize yet, quantization config is not changed yet
         let suggested_to_optimize =
-            config_mismatch_optimizer.check_condition(locked_holder.clone(), &Default::default());
+            config_mismatch_optimizer.plan_optimizations_for_test(&locked_holder);
         assert_eq!(suggested_to_optimize.len(), 0);
 
         // Create changed quantization config for vector2, update it in the optimizer
@@ -726,7 +716,8 @@ mod tests {
         // Run mismatch optimizer again, make sure it optimizes now
         let permit = budget.try_acquire(0, permit_cpu_count).unwrap();
         let suggested_to_optimize =
-            config_mismatch_optimizer.check_condition(locked_holder.clone(), &Default::default());
+            config_mismatch_optimizer.plan_optimizations_for_test(&locked_holder);
+        let suggested_to_optimize = suggested_to_optimize.into_iter().exactly_one().unwrap();
         assert_eq!(suggested_to_optimize.len(), 1);
         let changed = config_mismatch_optimizer
             .optimize(

lib/collection/src/collection_manager/optimizers/segment_optimizer.rs
@@ -1,6 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeMap, HashMap, HashSet};
 use std::ops::Deref;
 use std::path::Path;
+use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 
 use common::budget::{ResourceBudget, ResourcePermit};
@@ -12,7 +13,7 @@ use fs_err as fs;
 use io::storage_version::StorageVersion;
 use itertools::Itertools;
 use parking_lot::lock_api::RwLockWriteGuard;
-use parking_lot::{Mutex, RwLockUpgradableReadGuard};
+use parking_lot::{Mutex, RwLock, RwLockUpgradableReadGuard};
 use segment::common::operation_error::{OperationResult, check_process_stopped};
 use segment::common::operation_time_statistics::{
     OperationDurationsAggregator, ScopeDurationMeasurer,
@@ -34,6 +35,7 @@ use crate::collection_manager::holders::segment_holder::{
 use crate::config::CollectionParams;
 use crate::operations::config_diff::DiffConfig;
 use crate::operations::types::{CollectionError, CollectionResult};
+use crate::update_handler::Optimizer;
 
 const BYTES_IN_KB: usize = 1024;
 
@@ -77,12 +79,34 @@ pub trait SegmentOptimizer {
     /// Get thresholds configuration for the current optimizer
     fn threshold_config(&self) -> &OptimizerThresholds;
 
-    /// Checks if segment optimization is required
-    fn check_condition(
-        &self,
-        segments: LockedSegmentHolder,
-        excluded_ids: &HashSet<SegmentId>,
-    ) -> Vec<SegmentId>;
+    /// Find segments that require optimization and write them into `planner`.
+    fn plan_optimizations(&self, planner: &mut OptimizationPlanner);
+
+    /// Wrapper around [`SegmentOptimizer::plan_optimizations`].
+    /// Simplified interface and extra checks.
+    #[cfg(test)]
+    fn plan_optimizations_for_test(&self, segments: &LockedSegmentHolder) -> Vec<Vec<SegmentId>> {
+        let segments = segments.read();
+
+        let mut planner = OptimizationPlanner::new(segments.iter_original());
+        self.plan_optimizations(&mut planner);
+        let result = planner.into_scheduled_for_test();
+
+        // Verify consistency: re-planning with remaining segments should match tail
+        let mut remaining: BTreeMap<_, _> = segments.iter_original().collect();
+        for (i, batch) in result.iter().enumerate() {
+            for &id in batch {
+                remaining.remove(&id);
+            }
+            let mut planner =
+                OptimizationPlanner::new(remaining.iter().map(|(&id, &seg)| (id, seg)));
+            planner.extra = i + 1;
+            self.plan_optimizations(&mut planner);
+            assert_eq!(planner.into_scheduled_for_test(), &result[i + 1..]);
+        }
+
+        result
+    }
 
     fn get_telemetry_counter(&self) -> &Mutex<OperationDurationsAggregator>;
 
@@ -948,5 +972,81 @@ pub trait SegmentOptimizer {
         }
 
         Ok((optimized_segment, write_segments_guard))
+    }
+}
+
+pub struct OptimizationPlanner<'a> {
+    /// Segments that could be scheduled for optimization.
+    remaining: BTreeMap<SegmentId, &'a Arc<RwLock<Segment>>>,
+
+    /// The resulting optimization plan.
+    ///
+    /// Each entry contains
+    /// - a batch of segments to be optimized/merged into a new segment,
+    /// - an optional optimizer so you can call [`SegmentOptimizer::optimize`]
+    ///   on it later.
+    scheduled: Vec<(Option<Arc<Optimizer>>, Vec<SegmentId>)>,
+
+    /// Number of extra phantom segments that will be added to
+    /// [`Self::expected_segments_number`].
+    extra: usize,
+
+    /// This goes into [`Self::scheduled`].
+    current_optimizer: Option<Arc<Optimizer>>,
+}
+
+impl<'a> OptimizationPlanner<'a> {
+    pub fn new(segments: impl IntoIterator<Item = (SegmentId, &'a Arc<RwLock<Segment>>)>) -> Self {
+        Self {
+            remaining: segments.into_iter().collect(),
+            scheduled: Vec::new(),
+            extra: 0,
+            current_optimizer: None,
+        }
+    }
+
+    pub fn remaining(&self) -> &BTreeMap<SegmentId, &'a Arc<RwLock<Segment>>> {
+        &self.remaining
+    }
+
+    /// Should set called before calling [`Self::plan`] if you want the
+    /// optimizer to be attached to [`Self::scheduled`].
+    pub fn set_current_optimizer(&mut self, optimizer: Arc<Optimizer>) {
+        self.current_optimizer = Some(optimizer);
+    }
+
+    pub fn scheduled(&self) -> &Vec<(Option<Arc<Optimizer>>, Vec<SegmentId>)> {
+        &self.scheduled
+    }
+
+    pub fn into_scheduled(self) -> Vec<(Option<Arc<Optimizer>>, Vec<SegmentId>)> {
+        self.scheduled
+    }
+
+    #[cfg(test)]
+    pub fn into_scheduled_for_test(self) -> Vec<Vec<SegmentId>> {
+        self.scheduled
+            .into_iter()
+            .map(|(_, segments)| segments)
+            .collect_vec()
+    }
+
+    /// The expected resulting number of segments after the optimization plan is
+    /// executed.
+    pub fn expected_segments_number(&self) -> usize {
+        self.remaining.len() + self.scheduled.len() + self.extra
+    }
+
+    /// Schedule this batch of segments to be optimized/merged into new segment.
+    pub fn plan(&mut self, segments: Vec<SegmentId>) {
+        if segments.is_empty() {
+            debug_assert!(false);
+        }
+        for segment_id in &segments {
+            let removed = self.remaining.remove(segment_id).is_some();
+            debug_assert!(removed);
+        }
+        self.scheduled
+            .push((self.current_optimizer.clone(), segments));
     }
 }

BlackRoad-OS/qdrant-1

lib/collection/src/collection_manager/optimizers/config_mismatch_optimizer.rs
@@ -1,18 +1,16 @@
-use std::collections::HashSet;
+use std::cmp::Reverse;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
+use itertools::Itertools;
 use parking_lot::Mutex;
 use segment::common::operation_time_statistics::OperationDurationsAggregator;
 use segment::entry::SegmentEntry;
 use segment::index::sparse_index::sparse_index_config::SparseIndexType;
-use segment::types::{
-    HnswConfig, HnswGlobalConfig, Indexes, QuantizationConfig, SegmentType, VectorName,
-};
+use segment::types::{HnswConfig, HnswGlobalConfig, Indexes, QuantizationConfig, VectorName};
 
-use crate::collection_manager::holders::segment_holder::{LockedSegmentHolder, SegmentId};
 use crate::collection_manager::optimizers::segment_optimizer::{
-    OptimizerThresholds, SegmentOptimizer,
+    OptimizationPlanner, OptimizerThresholds, SegmentOptimizer,
 };
 use crate::config::CollectionParams;
 use crate::operations::config_diff::DiffConfig;
@@ -75,10 +73,6 @@ impl ConfigMismatchOptimizer {
 
     fn config_mismatch(&self, read_segment: &dyn SegmentEntry) -> bool {
         let segment_config = read_segment.config();
-
-        if read_segment.segment_type() == SegmentType::Special {
-            return false; // Never optimize already optimized segment
-        }
 
         if self.collection_params.on_disk_payload
             != segment_config.payload_storage_type.is_on_disk()
@@ -209,31 +203,25 @@ impl SegmentOptimizer for ConfigMismatchOptimizer {
         &self.thresholds_config
     }
 
-    fn check_condition(
-        &self,
-        segments: LockedSegmentHolder,
-        excluded_ids: &HashSet<SegmentId>,
-    ) -> Vec<SegmentId> {
-        segments
-            .read()
+    fn plan_optimizations(&self, planner: &mut OptimizationPlanner) {
+        let to_optimize = planner
+            .remaining()
             .iter()
-            // Excluded externally, might already be scheduled for optimization
-            .filter(|(segment_id, _segment)| !excluded_ids.contains(segment_id))
-            .filter_map(|(segment_id, segment)| {
-                let segment_entry = segment.get();
-                let read_segment = segment_entry.read();
-                self.config_mismatch(&*read_segment).then(|| {
-                    let vector_size = read_segment
+            .filter_map(|(&segment_id, segment)| {
+                let segment = segment.read();
+                self.config_mismatch(&*segment).then(|| {
+                    let vector_size = segment
                         .max_available_vectors_size_in_bytes()
                         .unwrap_or_default();
                     Some((segment_id, vector_size))
                 })?
             })
-            // Select segment with largest vector size
-            .max_by_key(|(_segment_id, vector_size)| *vector_size)
-            .map(|(segment_id, _vector_size)| segment_id)
-            .into_iter()
-            .collect()
+            // Segments with largest vector size come first
+            .sorted_by_key(|(_segment_id, vector_size)| Reverse(*vector_size))
+            .collect_vec();
+        for (segment_id, _) in to_optimize {
+            planner.plan(vec![segment_id]);
+        }
     }
 
     fn get_telemetry_counter(&self) -> &Mutex<OperationDurationsAggregator> {
@@ -365,7 +353,7 @@ mod tests {
 
         // Mismatch optimizer should not optimize yet, HNSW config is not changed yet
         let suggested_to_optimize =
-            config_mismatch_optimizer.check_condition(locked_holder.clone(), &Default::default());
+            config_mismatch_optimizer.plan_optimizations_for_test(&locked_holder);
         assert_eq!(suggested_to_optimize.len(), 0);
 
         // Create changed HNSW config with other m/ef_construct value, update it in the optimizer
@@ -377,7 +365,8 @@ mod tests {
         // Run mismatch optimizer again, make sure it optimizes now
         let permit = budget.try_acquire(0, permit_cpu_count).unwrap();
         let suggested_to_optimize =
-            config_mismatch_optimizer.check_condition(locked_holder.clone(), &Default::default());
+            config_mismatch_optimizer.plan_optimizations_for_test(&locked_holder);
+        let suggested_to_optimize = suggested_to_optimize.into_iter().exactly_one().unwrap();
         assert_eq!(suggested_to_optimize.len(), 1);
         let changed = config_mismatch_optimizer
             .optimize(
@@ -527,7 +516,7 @@ mod tests {
 
         // Mismatch optimizer should not optimize yet, HNSW config is not changed yet
         let suggested_to_optimize =
-            config_mismatch_optimizer.check_condition(locked_holder.clone(), &Default::default());
+            config_mismatch_optimizer.plan_optimizations_for_test(&locked_holder);
         assert_eq!(suggested_to_optimize.len(), 0);
 
         // Create changed HNSW config for vector2, update it in the optimizer
@@ -547,7 +536,8 @@ mod tests {
         // Run mismatch optimizer again, make sure it optimizes now
         let permit = budget.try_acquire(0, permit_cpu_count).unwrap();
         let suggested_to_optimize =
-            config_mismatch_optimizer.check_condition(locked_holder.clone(), &Default::default());
+            config_mismatch_optimizer.plan_optimizations_for_test(&locked_holder);
+        let suggested_to_optimize = suggested_to_optimize.into_iter().exactly_one().unwrap();
         assert_eq!(suggested_to_optimize.len(), 1);
         let changed = config_mismatch_optimizer
             .optimize(
@@ -703,7 +693,7 @@ mod tests {
 
         // Mismatch optimizer should not optimize yet, quantization config is not changed yet
         let suggested_to_optimize =
-            config_mismatch_optimizer.check_condition(locked_holder.clone(), &Default::default());
+            config_mismatch_optimizer.plan_optimizations_for_test(&locked_holder);
         assert_eq!(suggested_to_optimize.len(), 0);
 
         // Create changed quantization config for vector2, update it in the optimizer
@@ -726,7 +716,8 @@ mod tests {
         // Run mismatch optimizer again, make sure it optimizes now
         let permit = budget.try_acquire(0, permit_cpu_count).unwrap();
         let suggested_to_optimize =
-            config_mismatch_optimizer.check_condition(locked_holder.clone(), &Default::default());
+            config_mismatch_optimizer.plan_optimizations_for_test(&locked_holder);
+        let suggested_to_optimize = suggested_to_optimize.into_iter().exactly_one().unwrap();
         assert_eq!(suggested_to_optimize.len(), 1);
         let changed = config_mismatch_optimizer
             .optimize(

lib/collection/src/collection_manager/optimizers/segment_optimizer.rs
@@ -1,6 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeMap, HashMap, HashSet};
 use std::ops::Deref;
 use std::path::Path;
+use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 
 use common::budget::{ResourceBudget, ResourcePermit};
@@ -12,7 +13,7 @@ use fs_err as fs;
 use io::storage_version::StorageVersion;
 use itertools::Itertools;
 use parking_lot::lock_api::RwLockWriteGuard;
-use parking_lot::{Mutex, RwLockUpgradableReadGuard};
+use parking_lot::{Mutex, RwLock, RwLockUpgradableReadGuard};
 use segment::common::operation_error::check_process_stopped;
 use segment::common::operation_time_statistics::{
     OperationDurationsAggregator, ScopeDurationMeasurer,
@@ -34,6 +35,7 @@ use crate::collection_manager::holders::segment_holder::{
 use crate::config::CollectionParams;
 use crate::operations::config_diff::DiffConfig;
 use crate::operations::types::{CollectionError, CollectionResult};
+use crate::update_handler::Optimizer;
 
 const BYTES_IN_KB: usize = 1024;
 
@@ -77,12 +79,33 @@ pub trait SegmentOptimizer {
     /// Get thresholds configuration for the current optimizer
     fn threshold_config(&self) -> &OptimizerThresholds;
 
-    /// Checks if segment optimization is required
-    fn check_condition(
-        &self,
-        segments: LockedSegmentHolder,
-        excluded_ids: &HashSet<SegmentId>,
-    ) -> Vec<SegmentId>;
+    /// Find segments that require optimization and write them into `planner`.
+    fn plan_optimizations(&self, planner: &mut OptimizationPlanner);
+
+    /// Wrapper around [`SegmentOptimizer::plan_optimizations`].
+    /// Simplified interface and extra checks.
+    #[cfg(test)]
+    fn plan_optimizations_for_test(&self, segments: &LockedSegmentHolder) -> Vec<Vec<SegmentId>> {
+        let segments = segments.read();
+
+        let mut planner = OptimizationPlanner::new(0, segments.iter_original());
+        self.plan_optimizations(&mut planner);
+        let result = planner.into_scheduled_for_test();
+
+        // Verify consistency: re-planning with remaining segments should match tail
+        let mut remaining: BTreeMap<_, _> = segments.iter_original().collect();
+        for (i, batch) in result.iter().enumerate() {
+            for &id in batch {
+                remaining.remove(&id);
+            }
+            let mut planner =
+                OptimizationPlanner::new(i + 1, remaining.iter().map(|(&id, &seg)| (id, seg)));
+            self.plan_optimizations(&mut planner);
+            assert_eq!(planner.into_scheduled_for_test(), &result[i + 1..]);
+        }
+
+        result
+    }
 
     fn get_telemetry_counter(&self) -> &Mutex<OperationDurationsAggregator>;
 
@@ -922,5 +945,84 @@ pub trait SegmentOptimizer {
         }
 
         Ok((optimized_segment, write_segments_guard))
+    }
+}
+
+pub struct OptimizationPlanner<'a> {
+    /// Segments that could be scheduled for optimization.
+    remaining: BTreeMap<SegmentId, &'a Arc<RwLock<Segment>>>,
+
+    /// The resulting optimization plan.
+    ///
+    /// Each entry contains
+    /// - a batch of segments to be optimized/merged into a new segment,
+    /// - an optional optimizer so you can call [`SegmentOptimizer::optimize`]
+    ///   on it later.
+    scheduled: Vec<(Option<Arc<Optimizer>>, Vec<SegmentId>)>,
+
+    /// Amount of currently running optimizations. We'll assume that each of
+    /// them eventually produces one new segment.
+    current: usize,
+
+    /// This goes into [`Self::scheduled`].
+    optimizer: Option<Arc<Optimizer>>,
+}
+
+impl<'a> OptimizationPlanner<'a> {
+    pub fn new<I>(current: usize, segments: I) -> Self
+    where
+        I: IntoIterator<Item = (SegmentId, &'a Arc<RwLock<Segment>>)>,
+    {
+        Self {
+            remaining: segments.into_iter().collect(),
+            scheduled: Vec::new(),
+            current,
+            optimizer: None,
+        }
+    }
+
+    pub fn remaining(&self) -> &BTreeMap<SegmentId, &'a Arc<RwLock<Segment>>> {
+        &self.remaining
+    }
+
+    /// Should set called before calling [`Self::plan`] if you want the
+    /// optimizer to be attached to [`Self::scheduled`].
+    pub fn set_optimizer(&mut self, optimizer: Arc<Optimizer>) {
+        self.optimizer = Some(optimizer);
+    }
+
+    pub fn scheduled(&self) -> &Vec<(Option<Arc<Optimizer>>, Vec<SegmentId>)> {
+        &self.scheduled
+    }
+
+    pub fn into_scheduled(self) -> Vec<(Option<Arc<Optimizer>>, Vec<SegmentId>)> {
+        self.scheduled
+    }
+
+    /// Like [`Self::into_scheduled`], but drops the optimizers.
+    #[cfg(test)]
+    pub fn into_scheduled_for_test(self) -> Vec<Vec<SegmentId>> {
+        self.scheduled
+            .into_iter()
+            .map(|(_, segments)| segments)
+            .collect_vec()
+    }
+
+    /// The expected resulting number of segments after the optimization plan is
+    /// executed, and all currently running optimizations are finished.
+    pub fn expected_segments_number(&self) -> usize {
+        self.remaining.len() + self.scheduled.len() + self.current
+    }
+
+    /// Schedule this batch of segments to be optimized/merged into new segment.
+    pub fn plan(&mut self, segments: Vec<SegmentId>) {
+        if segments.is_empty() {
+            debug_assert!(false);
+        }
+        for segment_id in &segments {
+            let removed = self.remaining.remove(segment_id).is_some();
+            debug_assert!(removed);
+        }
+        self.scheduled.push((self.optimizer.clone(), segments));
     }
 }